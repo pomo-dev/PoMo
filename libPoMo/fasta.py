51c904ff235b8b905fb77319b147a093b2e6b242
Dominik Schrempf <dominik.schrempf@gmail.com>
2013-11-26 16:38:41 +0100

    Create fasta module.

---
+++ libPoMo/fasta.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""libPomo.fasta
+----------------------------------------------------------------------
+
+This module provides functions to read, write and access fasta files.
+
+"""
