d45b2beb5eb5d10a4368daeb4502e08d64d3ab3f
Dominik Schrempf <dominik.schrempf@gmail.com>
2013-11-27 17:21:49 +0100

    read in fasta files

---
+++ convertVCF.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""Convert VCF files to count format.
+
+This script converts a VCF file containing SNP data to a file in count
+format. A reference genome needs to be provided in fasta format as
+input. The SNP data will be merged with the reference genome to create
+a complete sequence that contains SNPs as well as unchanged bases
+(which are also informative for phylogenetic analysis).
+
+"""
+
+import argparse
+import libPoMo.fasta as fa
+
+ver = "1.0"
+
+parser = argparse.ArgumentParser(prog='convertVCF',
+                                 description="convertVCF script version" + ver)
+parser.add_argument('reference',
+                    help="path to reference genome in fasta format")
+parser.add_argument('vcf-file',
+                    help="path to vcf file with SNP information")
+args = parser.parse_args()
+
+(seqNames, seqDescr, seqData) = fa.open_fa(args.reference)
+print(seqNames)
+print(seqDescr)
+print(seqData)
--- libPoMo/fasta.py
+++ libPoMo/fasta.py
@@ -6,3 +6,95 @@
 This module provides functions to read, write and access fasta files.
 
 """
+
+
+class NotAFastaFileError(Exception):
+    """Exception raised if given fasta file is not valid."""
+    pass
+
+class SequenceDataError(Exception):
+    # TODO
+    pass
+
+
+def get_sp_name_and_description(fa_header_line):
+    """Extracts species name and description from a fasta file header line."""
+    linelist = fa_header_line.split()
+    name = linelist[0].replace(">", "")
+    description = ""
+    if len(linelist) > 1:
+        description = linelist[1]
+    return (name, description)
+
+
+def test_sequence(seqNames, seqData):
+    """Tests if sequences contain data."""
+    l = len(seqNames)
+    if l != len(seqData):
+        raise 
+
+def open_fa(faFileName, maxskip=50):
+    """Opens a fasta file.
+
+    This function tries to open the given fasta file, checks if it is
+    in fasta format and reads the sequence(s). It only looks `maxskip`
+    lines for the start of a sequence (defaults to 50). It returns a
+    tuple that contains a list of species names, a list of the
+    respective desriptions and a list with the sequences.
+
+    """
+    seqNames = []
+    seqDescr = []
+    seqData = []
+
+    flag = False
+    with open(faFileName) as faFile:
+        # Find the start of the first sequence.
+        for i in range(0, maxskip):
+            line = faFile.readline()
+            if line == '':
+                raise NotAFastaFileError("File contains no data.")
+            if line[0] == '>':
+                # species name found in line
+                flag = True
+                break
+        if flag is False:
+            raise NotAFastaFileError()
+        (name, desc) = get_sp_name_and_description(line)
+        seqNames.append(name)
+        seqDescr.append(desc)
+        data = ""
+        for line in faFile:
+            if line[0] == '>':
+                # new species found in line
+                seqData.append(data)
+                (name, desc) = get_sp_name_and_description(line)
+                seqNames.append(name)
+                seqDescr.append(desc)
+                data = ""
+                line = faFile.readline()
+            data += line.replace("\n", "")
+    seqData.append(data)
+    test_sequence(seqNames, seqData)
+    return (seqNames, seqDescr, seqData)
+
+
+# TODO
+def get_base(chrom, pos):
+    """Gets nucleotide base at position `pos` on chromosome `chrom`."""
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
