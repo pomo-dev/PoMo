e2d4397c34babf68e68e5c5b4dee5c175ed1d029
Dominik Schrempf <dominik.schrempf@gmail.com>
2016-01-18 12:16:07 +0100

    Remove node labels (#4).

--- sample-data/PoMo-TestRun.sh
+++ sample-data/PoMo-TestRun.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Do a PoMo test run.
+# Please adjust the file paths.
 
 Python=python
-PoMo=~/Population-Genetics/PoMo-git-repo/PoMo-Development/PoMo.py
-HYPHY=~/Population-Genetics/PoMo-git-repo/HYPHY/HYPHYMP
+PoMo=../PoMo.py
+HYPHY=../../HYPHY/HYPHYMP
 data=counts_file_sample.cf
 
 $Python $PoMo $HYPHY $data -v
